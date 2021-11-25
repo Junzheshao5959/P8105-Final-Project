9bbc7c330261dd901f82fd8d61b4e1fe350c0a0f
Yuchen Zheng <yuchenzheng@yuchens-mbp.dyn.nyp.org>
2021-11-24 22:53:49 -0500

    green taxi data cleaning

---
+++ data_cleaning.R
@@ -0,0 +1,51 @@
+library(data.table)
+library("dplyr")
+setDTthreads(threads = 12)# customize your threads
+# data discovery --------------------
+test_dt = fread("raw_data/data/fhv/2020_1.csv")
+zone_dt = fread("taxi+_zone_lookup (2).csv")
+location_list = zone_dt[Borough == "Manhattan"][,list(LocationID,Zone)]
+PUloation_list = location_list[,.(LocationID,PUZone = Zone)]
+DOloation_list = location_list[,.(LocationID,DOZone = Zone)]
+test_dt_zone = merge.data.table(test_dt, PUloation_list, by.y = "LocationID", by.x = "PULocationID")
+test_dt_zone = merge.data.table(test_dt_zone, DOloation_list, by.y = "LocationID", by.x = "DOLocationID")
+
+# read, clean, and sample -----------
+res_dt = data.table(matrix(ncol = 0, nrow = 0))
+#colnames(res_dt) = colnames(test_dt_zone)
+for (x in list.files("raw_data/data/fhv")){
+  temp_dt = fread(paste("raw_data/data/fhv/",x,sep =""))
+  temp_dt = merge.data.table(temp_dt, PUloation_list, by.y = "LocationID", by.x = "PULocationID")
+  temp_dt = merge.data.table(temp_dt, DOloation_list, by.y = "LocationID", by.x = "DOLocationID")
+  temp_dt = as.data.table(sample_n(as_tibble(temp_dt),round(nrow(temp_dt)/10)))# random sample 10%
+  res_dt = rbind(res_dt, temp_dt, fill = TRUE)
+  print(x)
+  print(nrow(temp_dt))
+  print(nrow(res_dt))
+}
+
+
+# green taxi --------------------
+gtaxi_test_dt = fread("raw_data/data/green_taxi/2020_1.csv")
+zone_dt = fread("taxi+_zone_lookup (2).csv")
+location_list = zone_dt[Borough == "Manhattan"][,list(LocationID,Zone)]
+PUloation_list = location_list[,.(LocationID,PUZone = Zone)]
+DOloation_list = location_list[,.(LocationID,DOZone = Zone)]
+gtest_dt_zone = merge.data.table(gtaxi_test_dt, PUloation_list, by.y = "LocationID", by.x = "PULocationID")
+gtest_dt_zone = merge.data.table(gtest_dt_zone, DOloation_list, by.y = "LocationID", by.x = "DOLocationID")
+
+# read, clean, and sample -----------
+gtaxi_res_dt = data.table(matrix(ncol = 0, nrow = 0))
+for (x in list.files("raw_data/data/green_taxi")){
+  gtaxi_dt = fread(paste("raw_data/data/green_taxi/", x, sep =""))
+  gtaxi_dt = merge.data.table(gtaxi_dt, PUloation_list, by.y = "LocationID", by.x = "PULocationID")
+  gtaxi_dt = merge.data.table(gtaxi_dt, DOloation_list, by.y = "LocationID", by.x = "DOLocationID")
+  gtaxi_dt = as.data.table(sample_n(as_tibble(gtaxi_dt),round(nrow(gtaxi_dt)/100)))# random sample 10%
+  gtaxi_res_dt = rbind(gtaxi_res_dt, gtaxi_dt, fill = TRUE)
+  #cat(colnames(gtaxi_res_dt))
+  print(x)
+  print(nrow(gtaxi_dt))
+  print(nrow(gtaxi_res_dt))
+}
+
+gtaxi_clean = gtaxi_res_dt[,c(1,2,4,5,9,21,22)]
